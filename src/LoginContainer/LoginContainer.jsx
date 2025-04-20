e5988198df990f884514e62ccb443d243924a106
Joseph Bloom <jsphbloom@gmail.com>
2025-04-20 10:36:33 -0600

    added form with input and submit button in LoginContainer

--- src/LoginContainer/LoginContainer.jsx
+++ src/LoginContainer/LoginContainer.jsx
@@ -3,10 +3,19 @@ import { NavLink } from 'react-router-dom'
 
 const LoginContainer = () => {
     return (
-        <nav>
-        <NavLink to="/" className="nav">Search</NavLink>
-        <NavLink to="/:user_id" className="nav">Login</NavLink>
-        </nav>
+        <div>
+
+            <nav>
+                <NavLink to="/" className="nav">Search</NavLink>
+                <NavLink to="/:user_id" className="nav">Login</NavLink>
+            </nav>
+
+            <form>
+                <input className ="userName"/>
+            <button type="submit">Submit</button>
+            </form>
+
+        </div>
     )
 }
 export default LoginContainer;

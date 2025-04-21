02362ec621b8d642c4f02b542ce64700abbe590d
little-Patrick <Little.patrick@outlook.com>
2025-04-21 12:49:43 -0600

    Refactor: NavLink for saving reports

--- src/UserReportsContainer/UserReportsContainer.jsx
+++ src/UserReportsContainer/UserReportsContainer.jsx
@@ -5,6 +5,8 @@ const UserReportsContainer = () => {
     return (
         <nav>
         <NavLink to="/:user_id" className="nav">Search</NavLink>
+        <NavLink to="/:user_id/:userReportsId" className="nav">Saved Reports</NavLink>
+
         <NavLink to="/" className="nav">Log out</NavLink>
         </nav>
     )

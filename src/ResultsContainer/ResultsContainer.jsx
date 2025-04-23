0a11f212b5b35784eb1ad3c34ecb6f960dd533ff
Mark Kendall <mkendall.tutor@gmail.com>
2025-04-23 18:06:17 -0500

    Add BE create report skeleton methods (not functioning yet)

--- src/ResultsContainer/ResultsContainer.jsx
+++ src/ResultsContainer/ResultsContainer.jsx
@@ -5,16 +5,45 @@ import useState from 'react';
 //Later: will also want to list average utility rates for the state - will make BE call to get this info (either its own controller, or another action from UtilitiesController)
 //For now: button will reside within this container (hopefully can format it to put it in the right place)
 
-const ResultsContainer = ({ results }) => {
+const ResultsContainer = ({ user, results }) => {
 
     // debugger
 
     let nickname = results.nickname
-    let nameAndLocation = `Your Estimated Energy Usage for ${nickname}`
+    let nameAndLocation = `Your Estimated Energy Usage and Costs for ${nickname}`
+    //Later: add zip code and state (based on BE return?)
+    //Later later: add utility company associated with this...
 
-    //Ensure proper display if erroneous / incopmlete results
+    //Ensure proper display if erroneous / incopmlete results (NOTE: still not displaying correctly on first page load; works after that)
     if (results === null || results.status === 422) {
         nameAndLocation = "Error and/or nothing searched for yet (placeholder)"
+    }
+    const saveResults = () => {
+        //Collect appropriate results to form JSON body
+        const reportData = {
+            nickname: results.nickname,
+            energy_consumption: results.energy_consumption,
+            cost: results.cost,
+            user_id: user.id                //This needs to be checked, this is just a placeholder (verify how we're getting the user info, is it an object/what) - user_id: user.id or similar
+        }
+        const parameters = {
+			method: "PATCH",
+			body: JSON.stringify(reportData),
+			headers: { "Content-Type": "application/json" }
+		}
+
+        //BE POST call to create new report
+        fetch("http://localhost:3000/api/v1/reports", parameters, {
+            //Maybe I don't even need these {} here?
+        })
+        .then(response => response.json())
+        .then(data => {
+            //Check successful; if so, provide text for updating save button and disable it (maybe call function here)
+        })
+        .catch(error => {
+            console.error("Error: ", error)
+            //Update the button appropriately with error / notification (same function?)
+        })
     }
 		
     return (
@@ -25,7 +54,7 @@ const ResultsContainer = ({ results }) => {
                 <p className='results'>{ `Energy cost: $${results.energy_consumption}` }</p>
                 <p className='results'>{ "Average state rate: <value goes here> $/kWh" }</p>
             </section>
-            <button>Save these results!</button>
+            <button onClick={() => saveResults()}>Save these results!</button>
         </section>
     )
 }

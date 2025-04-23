f8d25a228237eccfefb1e26e54514048e2f61677
little-Patrick <Little.patrick@outlook.com>
2025-04-22 19:02:54 -0600

    Add: User state

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react'
-import { NavLink, Route, Routes } from 'react-router-dom'
+import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import HomeContainer from './HomeContainer/HomeContainer'
 import './App.css'
 import UserReportsContainer from './UserReportsContainer/UserReportsContainer'
@@ -7,20 +7,20 @@ import ResultsContainer from './ResultsContainer/ResultsContainer'
 import LoginContainer from './LoginContainer/LoginContainer'
 
 function App() {
+	const [user, setUser] = useState(null)
 
-     return (
-     <main className='App'>
-        <h1>Watts My Electrical Bill</h1>
-        
-        <Routes>
-            <Route path="/" element={<HomeContainer />}/>
-               <Route path='/login' element={<LoginContainer />}/>
-            <Route path="/:user_id" element={<UserReportsContainer />}>
-                <Route path=":id" element={<ResultsContainer />}/>
-            </Route>
-        </Routes>
-     </main>
-          );
+	return (
+		<main className='App'>
+			<h1>Watts My Electrical Bill</h1>
+			<Routes>
+				<Route path="/" element={<HomeContainer user={user} />} />
+				<Route path='/login' element={<LoginContainer user={user} setUser={setUser} />} />
+				<Route path="/:report" element={<UserReportsContainer />}>
+					<Route path=":id" element={<ResultsContainer />} />
+				</Route>
+			</Routes>
+		</main>
+	);
 }
 
 export default App;

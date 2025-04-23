8808af553e9780add9451f45d582272480c79d05
Mark Kendall <mkendall.tutor@gmail.com>
2025-04-23 17:09:35 -0500

    Add placeholders and new structure for results, save button

--- src/ResultsContainer/ResultsContainer.jsx
+++ src/ResultsContainer/ResultsContainer.jsx
@@ -1,17 +1,31 @@
 import './ResultsContainer.css';
 import useState from 'react';
 
+//Need to add the "Save these results" button.  Should it live with this container, or outside of it?  (Kinda a matter of bookkeeping, but might need to nest a container)
+//Later: will also want to list average utility rates for the state - will make BE call to get this info (either its own controller, or another action from UtilitiesController)
+//For now: button will reside within this container (hopefully can format it to put it in the right place)
+
 const ResultsContainer = ({ results }) => {
-		let nickname = results.nickname
-		let nameAndLocation = `Your Estimated Energy Usage for ${nickname}`
+
+    // debugger
+
+    let nickname = results.nickname
+    let nameAndLocation = `Your Estimated Energy Usage for ${nickname}`
+
+    //Ensure proper display if erroneous / incopmlete results
+    if (results === null || results.status === 422) {
+        nameAndLocation = "Error and/or nothing searched for yet (placeholder)"
+    }
 		
     return (
         <section className='results-window'>
             <p>{nameAndLocation}</p>
-                <section className='values'>
-                    <p className='results'>{ results.cost }</p>
-                    <p className='results'>{ results.energy_consumption }</p>
-                </section>
+            <section className='values'>
+                <p className='results'>{ `Energy consumption: ${results.cost} kWh` }</p>
+                <p className='results'>{ `Energy cost: $${results.energy_consumption}` }</p>
+                <p className='results'>{ "Average state rate: <value goes here> $/kWh" }</p>
+            </section>
+            <button>Save these results!</button>
         </section>
     )
 }

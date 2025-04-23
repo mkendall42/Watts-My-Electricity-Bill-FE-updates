d7d9525fa45af4617f3e3a6a2d31ca6e82279d63
little-Patrick <Little.patrick@outlook.com>
2025-04-22 20:54:43 -0600

    Add: user state to pass down as prop to UserReportsContainer

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,8 @@ function App() {
 				<Route path="/" element={<HomeContainer user={user} />} />
 				<Route path='/login' element={<LoginContainer user={user} setUser={setUser} />} />
 				<Route path='/user' element={<UserContainer />} />
-				<Route path="/reports" element={<UserReportsContainer />}>
-					<Route path=":id" element={<ResultsContainer />} />
+				<Route path="/reports" element={<UserReportsContainer user={user}/>}>
+					<Route path=":id" element={<ResultsContainer user={user} />} />
 				</Route>
 			</Routes>
 		</main>

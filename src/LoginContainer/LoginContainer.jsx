76102debec4d1f564eb8c8d3be4185ce982a649d
Mark Kendall <mkendall.tutor@gmail.com>
2025-04-24 13:17:45 -0500

    Update login to find user ID, navigate to another page (functional, but needs updates)

--- src/LoginContainer/LoginContainer.jsx
+++ src/LoginContainer/LoginContainer.jsx
@@ -1,5 +1,5 @@
 import './LoginContainer.css'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 import DropdownMenuContainer from '../DropdownMenuContainer/DropdownMenuContainer';
 import { useState } from 'react'
 
@@ -7,6 +7,8 @@ import { useState } from 'react'
 const LoginContainer = () => {
     const [userItems, setUserItems] = useState(null)
     const [selectedUser, setSelectedUser] = useState(null)
+
+    const navigateToPage = useNavigate()
 
     //Get all usernames for the dropdown
     //NOTE: later, if we have an empty user list, should probably give message before rendering dropdown (this is also why 'null' vs '[]' used initially)
@@ -17,11 +19,12 @@ const LoginContainer = () => {
         .then(response => response.json())
         .then(data => {
             //Update userItems (note that JSON top-level is not usual {} but instead []) - had to do via function, apparently userItems isn't in scope???
-            let mappedUserItems = data.map((user) => {
-                return user.username
-            })
+            // let mappedUserItems = data.map((user) => {
+            //     return user.username
+            // })
 
-            setUserItems(mappedUserItems)
+            // setUserItems(mappedUserItems)
+            setUserItems(data)                      //This preserves the id associated with the name, which we need once a user is selected to login
         })
         .catch(error => {
             console.error("Error: ", error)
@@ -32,6 +35,28 @@ const LoginContainer = () => {
         //Not exactly sure how to best handle this...
         console.log("Newly selected user: ", username)
         setSelectedUser(username)
+    }
+
+    const processLogin = () => {
+        //Set the user state var (how do we get the setUser() prop sent here from HomeContainer via NavLink / whatever/)
+        console.log("Logging in user: ", selectedUser)
+        console.log(`${selectedUser}'s ID: `, findUserId(selectedUser))
+        //setUser(selectedUser)
+        navigateToPage(`/${findUserId(selectedUser)}`)       //Is this where we want to go (ReportsContainer)?  Also, once other actions taken, user is lost (need to set state somehow I'm guessing)
+    }
+
+    const findUserId = (username) => {
+        return userItems.find((user) => {
+            return user.username === username
+        }).id
+    }
+
+    //Need the if since call was async
+    let usernames = []
+    if (userItems) {
+        usernames = userItems.map((user) => {
+            return user.username
+        })
     }
 
     return (
@@ -46,7 +71,7 @@ const LoginContainer = () => {
                 {"User:  "} 
             </p>
             {userItems && (
-                <DropdownMenuContainer key="login-menu" itemsList={userItems} defaultText="Select username" processSelection={processUserSelection} />
+                <DropdownMenuContainer key="login-menu" itemsList={usernames} defaultText="Select username" processSelection={processUserSelection} />
             )}
             <br></br>
 
@@ -57,7 +82,7 @@ const LoginContainer = () => {
             )} */}
             {/* <button disabled={!selectedUser} onClick={() => console.log("Selected user: ", selectedUser)}>Login!</button> */}
             {selectedUser ? (
-                <button onClick={() => console.log("Selected user: ", selectedUser)}>Login!</button>
+                <button onClick={() => processLogin()}>Login!</button>
             ) : (
                 <button className="button-disabled" disabled={true}>Pick a user...</button>
             )}

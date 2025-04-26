79a85fb1139c27629f317f2f7ed3d0e2b3db4935
Mark Kendall <mkendall.tutor@gmail.com>
2025-04-26 01:15:13 -0500

    Add uniqueness nickname check in SearchContainer

--- src/SearchContainer/SearchContainer.jsx
+++ src/SearchContainer/SearchContainer.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './SearchContainer.css';
 
-const SearchContainer = ({ setResults }) => {
+const SearchContainer = ({ user, setResults }) => {
 	const [nickname, setNickname] = useState('');
 	const [zipcode, setZipcode] = useState('');
   // const [lat, setLat] = useState('');
@@ -9,6 +9,7 @@ const SearchContainer = ({ setResults }) => {
   const [residenceType, setResidenceType] = useState('');
   const [occupants, setOccupants] = useState('');
   const [energyUsage, setEnergyUsage] = useState('');
+  const [reportNicknames, setReportNicknames] = useState([]);
 
   //IMPORTANT NOTE: this next line is important to set for ensuring the ResultsContainer gives the empty response.
   //HOWEVER, it seems to be occassionally (not nearly always) cause an error:
@@ -16,10 +17,35 @@ const SearchContainer = ({ setResults }) => {
   //No idea why it only sometimes triggers, and the page still seems to load correctly as far as I can tell...
   // setResults(null)
 
+  //If user is logged in, load all report nicknames for that user to have on hand for checking uniqueness upon form submission
+  // let reportNicknames = []
+  useEffect(() => {
+    if (user !== '') {
+      fetch(`http://localhost:3000/api/v1/users/${user}/reports`)
+        .then(res => res.json())
+        .then(data => {
+          console.log("Loading existing report nicknames")
+          let nicknames = data.map((report) => {
+            return report.nickname
+          })
+
+          setReportNicknames(nicknames)
+        })
+        .catch(err => console.error('Error fetching reports:', err));
+    }
+  }, [])
+
   function submitSearch(event) {
     event.preventDefault();
 
-    // debugger
+    //Could we hijack this to first check to see if the nickname is unique, assuming a user is logged in?
+    //This way, we could abort the search before running external API call(s); also, simplifies code in ResultsContainer
+    //Check that nickname is unique, and abort now if not!
+    if (user !== '' && reportNicknames.includes(nickname)) {
+      console.log("Error: attempted duplicate nickname; try again!")
+      //NOTE: we should also provide user feedback here!
+      return
+    }
 
 		const queryParams = new URLSearchParams({
 			nickname: nickname,

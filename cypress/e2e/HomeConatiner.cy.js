a262c9b753f8e2e887196df1311a3cfc8670914a
little-Patrick <Little.patrick@outlook.com>
2025-04-26 11:43:24 -0600

    Add: user story for results container and login

--- cypress/e2e/HomeConatiner.cy.js
+++ cypress/e2e/HomeConatiner.cy.js
@@ -1,10 +1,5 @@
-describe('HomeContainer', () => {
+describe('HomeContainer and ResultsContainer', () => {
 	beforeEach(() => {
-		// cy.intercept("GET", "http://localhost:3001/api/v1/utilities", {
-		//   statusCode: 200,
-		//   fixture: "results"
-		// })
-
 		cy.visit('http://localhost:5173')
 	})
 
@@ -12,33 +7,46 @@ describe('HomeContainer', () => {
 		cy.get('h1').should('contain', 'Watts My Electrical Bill')
 	})
 
-	it('displays the form ', () => {
+	it('displays the form with all expected inputs', () => {
 		cy.get('form').should('exist')
 		cy.get('form input[name="nickname"]').should('exist')
-		cy.get('form input[name="lat"]').should('exist')
-		cy.get('form input[name="long"]').should('exist')
+		cy.get('form input[name="zipcode"]').should('exist')
 		cy.get('form input[name="residenceType"]').should('exist')
 		cy.get('form input[name="occupants"]').should('exist')
 		cy.get('form input[name="energyUsage"]').should('exist')
 		cy.get('form button').should('exist')
 	})
 
-	it('ResultsContainer', () => {
-	  cy.get('.results').should('exist')
-	  .get('p').first().should('have.text', 'Your Estimated Energy Usage for undefined')
-	  .get('.results').first().should('have.text', '')
-	  .get('.results').last().should('have.text', '')
+	it('displays default ResultsContainer message when no search submitted', () => {
+		cy.get('.results-window')
+			.should('exist')
+			.within(() => {
+				cy.contains('Results will appear here once you submit a valid search or view a saved report!')
+			})
 	})
 
-	it('adds a new idea to the list', () => {
-		cy.get('form').should('exist')
-		cy.get('form input[name="nickname"]').should('exist')
-		cy.get('form input[name="lat"]').should('exist')
-		cy.get('form input[name="long"]').should('exist')
-		cy.get('form input[name="residenceType"]').should('exist')
-		cy.get('form input[name="occupants"]').should('exist')
-		cy.get('form input[name="energyUsage"]').should('exist')
-		cy.get('form button').should('exist')
+	it('submits the form and displays updated results', () => {
+		cy.get('input[name="nickname"]').type('Test Home')
+		cy.get('input[name="zipcode"]').type('80236')
+		cy.get('input[name="residenceType"]').type('apartment')
+		cy.get('input[name="occupants"]').type('2')
+		cy.get('input[name="energyUsage"]').type('1')
+
 		cy.get('form button').click()
+
+		cy.get('.results-window').within(() => {
+			cy.get('p').first().should('contain', 'Your Estimated Energy Usage and Costs for "Test Home"')
+			cy.get('button').should('exist')
+		})
+	})
+
+	it('navigates to the login page when clicking the Login link', () => {
+		cy.get('nav').within(() => {
+			cy.contains('Login').click()
+		})
+
+		cy.url().should('include', '/login')
+		cy.get('.loginContainer').should('exist')
+		cy.contains('User:')
 	})
 })

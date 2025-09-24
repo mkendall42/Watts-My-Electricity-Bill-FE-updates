8da64ef7f7ec8417442f16eec58fdb3e748ed999
Mark Kendall <mkendall.tutor@gmail.com>
2025-09-24 02:32:32 -0500

    Fix: UserContainer: match formatting with main (HomeContainer) page

--- src/UserContainer/UserContainer.jsx
+++ src/UserContainer/UserContainer.jsx
@@ -6,28 +6,56 @@ import ResultsContainer from '../ResultsContainer/ResultsContainer'
 const UserContainer = ({ results, user,  setResults, isNewSearch, setIsNewSearch }) => {
 
 	return (
-		<div className='UserContainer'>
-			<div className="left-side">
-				<nav>
-					<NavLink to={`/user/${user}`} className="nav">Search</NavLink>
-					<NavLink to={`/user/${user}/saved`} className="nav">Saved Reports</NavLink>
-					<Link to="/" className="nav">Log out</Link>
-				</nav>
+		// <div className='UserContainer'>
+		// 	<div className="left-side">
+		// 		<nav>
+		// 			<NavLink to={`/user/${user}`} className="nav">Search</NavLink>
+		// 			<NavLink to={`/user/${user}/saved`} className="nav">Saved Reports</NavLink>
+		// 			<Link to="/" className="nav">Log out</Link>
+		// 		</nav>
 
+		// 		<SearchContainer
+		// 			user={user}
+		// 			setResults={setResults}
+		// 			isNewSearch={isNewSearch}
+		// 			setIsNewSearch={setIsNewSearch}
+		// 		/>
+		// 	</div>
+
+		// 	<ResultsContainer
+		// 		user={user}
+		// 		results={results}
+		// 		isNewSearch={isNewSearch}
+		// 		setIsNewSearch={setIsNewSearch}
+		// 	/>
+		// </div>
+
+		<div className='UserContainer'>
+			{/* <div className="left-side"> */}
+			<nav>
+				{/* <NavLink to="/" className="nav">Search</NavLink>
+				<NavLink to="/login" className="nav">Login</NavLink> */}
+				<NavLink to={`/user/${user}`} className="nav">Search</NavLink>
+				<NavLink to={`/user/${user}/saved`} className="nav">Saved Reports</NavLink>
+				<Link to="/" className="nav">Log out</Link>
+			</nav>
+
+			<div className="left-right-sides">
 				<SearchContainer
 					user={user}
 					setResults={setResults}
 					isNewSearch={isNewSearch}
 					setIsNewSearch={setIsNewSearch}
 				/>
-			</div>
+			{/* </div> */}
 
-			<ResultsContainer
-				user={user}
-				results={results}
-				isNewSearch={isNewSearch}
-				setIsNewSearch={setIsNewSearch}
-			/>
+				<ResultsContainer
+					user={user}
+					results={results}
+					isNewSearch={isNewSearch}
+					setIsNewSearch={setIsNewSearch}
+				/>
+			</div>
 		</div>
 	)
 }

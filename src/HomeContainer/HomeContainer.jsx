1be9ed0309a68bf1afcb95f457752caac42788f0
Mark Kendall <mkendall.tutor@gmail.com>
2025-09-24 02:00:58 -0500

    Fix: further App, HomeContainer alignment and formatting adjustments

--- src/HomeContainer/HomeContainer.jsx
+++ src/HomeContainer/HomeContainer.jsx
@@ -7,26 +7,28 @@ import { useState, useEffect } from 'react'
 function HomeContainer({ user, results, setResults, isNewSearch, setIsNewSearch }) {
 	return (
 		<div className='HomeContainer'>
-			<div className="left-side">
-				<nav>
-					<NavLink to="/" className="nav">Search</NavLink>
-					<NavLink to="/login" className="nav">Login</NavLink>
-				</nav>
+			{/* <div className="left-side"> */}
+			<nav>
+				<NavLink to="/" className="nav">Search</NavLink>
+				<NavLink to="/login" className="nav">Login</NavLink>
+			</nav>
 
+			<div className="left-right-sides">
 				<SearchContainer
 					user={user}
 					setResults={setResults}
 					isNewSearch={isNewSearch}
 					setIsNewSearch={setIsNewSearch}
 				/>
-			</div>
+			{/* </div> */}
 
-			<ResultsContainer
-				user={user}
-				results={results}
-				isNewSearch={isNewSearch}
-				setIsNewSearch={setIsNewSearch}
-			/>
+				<ResultsContainer
+					user={user}
+					results={results}
+					isNewSearch={isNewSearch}
+					setIsNewSearch={setIsNewSearch}
+				/>
+			</div>
 		</div>
 	);
 }

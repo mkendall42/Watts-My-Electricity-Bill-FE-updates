538a06058ff802a9390b382f90177c44011d9376
Mark Kendall <mkendall.tutor@gmail.com>
2025-09-30 23:15:56 -0500

    Feature: UserContainer now renders search and saved reports via Outlet

--- src/UserContainer/UserContainer.jsx
+++ src/UserContainer/UserContainer.jsx
@@ -1,6 +1,6 @@
-import { useParams, NavLink, Link } from 'react-router-dom'
+import { useParams, NavLink, Link, Outlet } from 'react-router-dom'
 import './UserContainer.css'
-import SearchContainer from '../SearchContainer/SearchContainer'
+// import SearchContainer from '../SearchContainer/SearchContainer'
 import ResultsContainer from '../ResultsContainer/ResultsContainer'
 
 const UserContainer = ({ results, user,  setResults, isNewSearch, setIsNewSearch }) => {
@@ -8,25 +8,15 @@ const UserContainer = ({ results, user,  setResults, isNewSearch, setIsNewSearch
 	return (
 		<div className='UserContainer'>
 			<nav>
-				<NavLink to={`/user/${user.id}`} className="nav-item">Search</NavLink>
+				<NavLink to={`/user/${user.id}/search`} className="nav-item">Search</NavLink>
 				<NavLink to={`/user/${user.id}/saved`} className="nav-item">Saved Reports</NavLink>
 				<Link to="/" className="nav-item">Log out</Link>
 			</nav>
 
 			<div className="left-right-sides">
-				<SearchContainer
-					user={user}
-					setResults={setResults}
-					isNewSearch={isNewSearch}
-					setIsNewSearch={setIsNewSearch}
-				/>
+				<Outlet />
 
-				<ResultsContainer
-					user={user}
-					results={results}
-					isNewSearch={isNewSearch}
-					setIsNewSearch={setIsNewSearch}
-				/>
+				<ResultsContainer user={user} results={results} isNewSearch={isNewSearch} setIsNewSearch={setIsNewSearch} />
 			</div>
 		</div>
 	)
